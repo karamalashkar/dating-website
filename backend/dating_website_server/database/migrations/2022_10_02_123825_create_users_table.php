3db18f4b738f67190ab20b7ab0cb467f40561695
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-02 16:46:34 +0300

    Backend <> Adding database tables

---
+++ backend/dating_website_server/database/migrations/2022_10_02_123825_create_users_table.php
@@ -0,0 +1,75 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("email");
+            $table->string("password");
+            $table->string("picture");
+            $table->string("bio");
+            $table->string("age");
+            $table->string("gender");
+            $table->string("interest");
+            $table->string("location");
+            $table->timestamps();
+        });
+
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->timestamps();
+        });
+
+        Schema::create('genders', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->timestamps();
+        });
+
+        Schema::create('blocks', function (Blueprint $table) {
+            $table->id();
+            $table->integer("user_id");
+            $table->integer("block_id");
+            $table->timestamps();
+        });
+
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('favorite_id');
+            $table->timestamps();
+        });
+
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->integer('sender_id');
+            $table->integer('receiver_id');
+            $table->string('message');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
