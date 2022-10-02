eb404b6b0c36efe5aba6e0c36995b9571cecc2c0
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-10-02 10:18:10 +0300

    Frontend <> Connecting every page in javascript

---
+++ frontend/scripts/script.js
@@ -0,0 +1,12 @@
+const loadFor = (page) => {
+    eval("load_" + page + "();");
+}
+
+//landing page
+const load_landing = async () => {}
+
+//home page
+const load_home = async () =>{}
+
+//chat page
+const load_chat = async () => {}
